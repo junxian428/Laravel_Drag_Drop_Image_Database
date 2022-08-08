3d1ce47a373949e276022be5d372ee5a29c176bb
junxian428 <holeyiho@gmail.com>
2022-08-08 19:20:58 +0800

    redis library imported

--- config/database.php
+++ config/database.php
@@ -93,6 +93,25 @@ return [
             // 'trust_server_certificate' => env('DB_TRUST_SERVER_CERTIFICATE', 'false'),
         ],
 
+        'redis' => [
+
+            'client' => env('REDIS_CLIENT', 'predis'),
+          
+            'options' => [
+                'cluster' => env('REDIS_CLUSTER', 'redis'),
+                'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
+            ],
+          
+            'default' => [
+                'url' => env('REDIS_URL'),
+                'host' => env('REDIS_HOST', '127.0.0.1'),
+                'password' => env('REDIS_PASSWORD', null),
+                'port' => env('REDIS_PORT', '6379'),
+                'database' => env('REDIS_DB', '0'),
+            ],
+          
+        ],
+
     ],
 
     /*
